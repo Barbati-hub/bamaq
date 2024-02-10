4b0788c835ca90a2f6f6712a54a7524ebdd775ac
Douglas A. Barbati <douglas-ltda@hotmail.com.br>
2024-02-10 00:21:08 -0400

    Update ClienteController.js

--- backend/controllers/ClienteController.js
+++ backend/controllers/ClienteController.js
@@ -5,7 +5,7 @@ const { createPool } = require('mysql2/promise');
 const pool = createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Dd@84255798',
+    password: '',
     database: 'bamaq'
 });
 
